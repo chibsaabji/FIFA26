9516795de38a10ab4ab3f39dccbf7915d12f1cb9
Vanshveer Chib <chibvanshveer@gmail.com>
2026-06-09 14:58:16 +0300

    Configure GitHub Pages deployment via Actions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
- assetsInclude: ['**/*.glb', '**/*.gltf']
+  base: '/FIFA26/',
+  assetsInclude: ['**/*.glb', '**/*.gltf']
 });
